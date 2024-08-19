cc5acd4156b5d573a9b9799e815750b751b064cd
Guitt0r <andriyka35@gmail.com>
2024-08-19 05:21:07 +0300

    fix:change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn2.thedogapi.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn2.thecatapi.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/cats",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
